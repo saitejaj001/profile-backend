5f988411688ec8f763a7a79e012e10368ffee896
sai.jn <Sai.Jn@ril.com>
2024-02-10 14:23:22 +0530

    get skills basic

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const router = require('./routes')
 const PORT = process.env["PORT"] || 8080
 const app = express()
 
+app.use(express.json())
+
 app.use('/api', router)
 
 app.listen(PORT, () =>{
--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,9 +1,17 @@
 const {Router}  = require('express')
+const mongoose = require('../repo')
+const {getAllSkills} = require('../repo/profile.repo')
 
 const router = Router()
 
 router.get('/', (req,res) =>{
     res.send('Hello world')
+})
+
+router.get('/getAllSkills', async (req, res) => {
+    getAllSkills().then((d) =>{
+        res.send(d)
+    })
 })
 
 module.exports = router;
